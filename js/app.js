8eebd29163eb8df122ccccb356ae85430f954889
jim9112 <jim9112@gmail.com>
2018-11-20 15:55:47 -0500

    working after changing scoring function

--- js/app.js
+++ js/app.js
@@ -6,25 +6,18 @@
     const boxes = document.querySelector('.boxes');
     const box = document.getElementsByClassName("box");
     var trackIfPlayer1Turn = true;
-    // winning combination 
-    const winningNumbers = [0, 1, 2, 0, 3, 6, 3, 4, 5, 6, 7, 8, 1, 4, 7, 2, 5, 8, 0, 4, 8, 2, 4, 6];
-    let tempArr = [];
     // An array of objects that save data about the two players
-    let players = [{
-            playerName: '',
-            playerPiece: 'o',
-            playerClass: 'box box-filled-1',
-            playerScore: [0, 0, 0, 0, 0, 0, 0, 0, 0]
-        },
-        {
-            playerName: '',
-            playerPiece: 'x',
-            playerClass: 'box box-filled-2',
-            playerScore: [0, 0, 0, 0, 0, 0, 0, 0, 0]
-        }
-    ];
+    const winningNumbers = [0, 1, 2, 0, 3, 6, 3, 4, 5, 6, 7, 8, 1, 4, 7, 2, 5, 8, 0, 4, 8, 2, 4, 6];
+    const playerData = {
+        player1Class: 'box box-filled-1',
+        player1Score: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        player2Class: 'box box-filled-2',
+        player2Score: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    };
+    
     // Obj handles all content changes for all screens
     const screens = {
+        // this section olds info needed to generate start win and tie screens
         startScreen: {
             setClass: 'screen screen-start',
             setID: 'start',
@@ -43,8 +36,9 @@
         tie: {
             setClass: 'screen screen-win screen-win-tie',
             setID: 'finish',
-            setContent: '<header> <h1>Tic Tac Toe</h1> <p class="message"> Tie!!!</p> <a href="#" class="button">New game</a> </header>'
+            setContent: '<header> <h1>Tic Tac Toe</h1> <p class="message"> Its a Draw!!!</p> <a href="#" class="button">New game</a> </header>'
         },
+        // This section handles funtion that change screen elements
         // Creates div for screen changes
         createConstructor: () => {
             const newScreen = document.createElement('div');
@@ -71,19 +65,22 @@
 
     const gameControls = {
         // Sets start player
-        beginGame: () => player1.className = 'players active',
+        beginGame: () => {player1.className = 'players active';
+        gameControls.resetGame();
+    },
         // Function checks the board and adjusts each players score
         scoring: () => {
             let trackTurns = 0;
             for (i = 0; i < box.length; i++) {      
                 if (box[i].className === 'box box-filled-1') {
-                    players[0].playerScore[i] = 1;
+                    playerData.player1Score[i] = 1;
                     trackTurns += 1;
                 } else if (box[i].className === 'box box-filled-2') {
-                    players[1].playerScore[i] = 1;
+                    playerData.player2Score[i] = 1;
                     trackTurns += 1;
                 }   
             }
+            // triggers tie screen if all spots are used and no winner
             if (trackTurns === 9){
                 screens.changeElement(board, 'none');
                 screens.createPage(screens.tie.setClass, screens.tie.setID, screens.tie.setContent);
@@ -93,33 +90,32 @@
         },
         // checks to see if current player has won
         checkForWin: (player) => {
-            for (i = 0; i < winningNumbers.length; i = i + 3) {
-                let answerArr = [winningNumbers[i], winningNumbers[i + 1], winningNumbers[i + 2]];
-                tempArr = [players[player].playerScore[answerArr[0]], players[player].playerScore[answerArr[1]], players[player].playerScore[answerArr[2]]];
-                if (tempArr[0] === 1 && tempArr[1] === 1 && tempArr[2] === 1) {
+            for (i = 0; i < winningNumbers.length; i = i + 3) {  
+                var k = [winningNumbers[i],winningNumbers[i+1],winningNumbers[i+2]];
+                if (player[k[0]] === 1 && player[k[[1]]] === 1 && player[k[2]] === 1) {
                     screens.changeElement(board, 'none');
-                    if (player === 0){
+                    if (player === playerData.player1Score){
                         screens.createPage(screens.oWin.setClass, screens.oWin.setID, screens.oWin.setContent);
                     } else {
                         screens.createPage(screens.xWin.setClass, screens.xWin.setID, screens.xWin.setContent);
                     }
-                    gameControls.resetGame();
+                    // gameControls.resetGame();
                     eventListener.startButton();
-                }
+                } 
             }
         },
         // resets game parameters
         resetGame: () => {
-            players[0].playerScore = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-            players[1].playerScore = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+            playerData.player1Score = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+            playerData.player2Score = [0, 0, 0, 0, 0, 0, 0, 0, 0];
             trackIfPlayer1Turn = true;
+            player2.className = 'players';
             for (i=0; i<box.length; i++){
                 box[i].className = 'box';
-                player2.className = 'players';
             }
         }
     };
-    // Object handles event listeners
+    // Object handles event listeners that need to be triggered
     const eventListener = {
         // event listener for start button
         startButton: () => {
@@ -146,14 +142,14 @@
             player1.className = 'players';
             player2.className = 'players active';
             gameControls.scoring();
-            gameControls.checkForWin(0);
+            gameControls.checkForWin(playerData.player1Score);
         } else {
             e.target.className = 'box box-filled-2';
             trackIfPlayer1Turn = true;
             player2.className = 'players';
             player1.className = 'players active';
             gameControls.scoring();
-            gameControls.checkForWin(1);
+            gameControls.checkForWin(playerData.player2Score);
         }
     });
 
